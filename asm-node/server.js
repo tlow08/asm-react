3e040b77343b12f1cee92d1edd4569cd35bb16fe
trantailoi <tailoitran08@gmail.com>
2024-09-22 16:30:46 +0700

    dbconfig

--- asm-node/server.js
+++ asm-node/server.js
@@ -2,34 +2,19 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import router from "./src/routers/index.js";
+import connectMongoDB from "./config/dbconfig.js";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-mongoose
-  .connect(`mongodb://localhost:27017/asm-react-fall24`)
-  .then(() => {
-    console.log("connect database thanh cong!");
+app.use(
+  express.urlencoded({
+    extended: true,
   })
-  .catch((err) => console.log(err));
+);
+app.use(express.json());
+connectMongoDB(`mongodb://localhost:27017/asm-react-fall24`);
+
 app.use("/api", router);
 
-const errorNotFound = (req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
-};
-
-const errorCommon = (err, req, res, next) => {
-  return res.status(err.status || 500).json({
-    message: err.message || "Error server",
-  });
-};
-
-app.use(errorNotFound, errorCommon);
-
-app.listen(8000, () => {
-  `Server is running on port 8000`;
-});
+app.listen(8000);
+export const viteNodeApp = app;
